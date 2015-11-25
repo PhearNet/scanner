6459bd566dcfaefe91e328a9bd737934bfcc8419
Michael Feher <git@phearzero.com>
2015-11-25 13:41:39 -0600

    Fixing wrong require in app

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var fs      = require('fs');
 //console.log(process.env);
 //var App = require('./src/Controller/Scanner');
-var App = require('./App');
+var App = require('./src/App');
 
 
 var http = require('http');
