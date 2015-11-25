3d72b64370aef061355e646d7faa90f8db7a3af9
Michael Feher <git@phearzero.com>
2015-11-24 21:15:32 -0600

    Adding debug log for env vars

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
-
+require('./src/Controller/Scanner');
 var http = require('http');
 /**
  *  Define the sample application.
@@ -24,6 +24,7 @@ var SampleApp = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
         self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        console.log(process.env);
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
