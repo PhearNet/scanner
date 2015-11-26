fc9c863e868f3658a3d513384676c8a6bc64306d
Michael Feher <git@phearzero.com>
2015-11-26 01:02:20 -0600

    Adding chmods, symlinks for nmap

--- src/Controller/Scanner.js
+++ src/Controller/Scanner.js
@@ -82,7 +82,10 @@ function isItUp(data) {
     if(process.env.OPENSHIFT_DATA_DIR){
         console.log(process.env.PATH);
         //opts.nmap = "."+ process.env.OPENSHIFT_REPO_DIR + "bin/nmap-openshift/nmap";
+
         fs.chmodSync(process.env.OPENSHIFT_REPO_DIR + "bin/nmap-openshift/nmap", 0775);
+        fs.linkSync(process.env.OPENSHIFT_REPO_DIR + "bin/nmap-openshift/nmap", process.env.OPENSHIFT_REPO_DIR +"node_modules/.bin/nmap");
+        fs.chmodSync(process.env.OPENSHIFT_REPO_DIR + "node_modules/.bin/nmap", 0775);
         console.log("OPTS", opts.nmap);
     }
 
