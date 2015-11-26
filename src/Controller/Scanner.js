501dc65578f05738e8234cd213dcf6be98702ece
Michael Feher <git@phearzero.com>
2015-11-26 02:16:54 -0600

    Deploy

--- src/Controller/Scanner.js
+++ src/Controller/Scanner.js
@@ -41,8 +41,8 @@ App.Store.queue.process('418b3a196f9a8a9fb526e9be3f5e6b35', function(job, done){
                     //TODO: Add validation
                     App.Store.conn.targets.merge(addr, report[addr], function(err, res){
                         if(err){
-                            done(new Error(err));
-                            console.log('Error in Store Merge', err);
+                            done(new Error(err,addr,report[addr]));
+                            console.log('Error in Store Merge', err, addr, report[addr]);
                             throw new Error(err);
                         } else targetblocksuccess++
                     });
